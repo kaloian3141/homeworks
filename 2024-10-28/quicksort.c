e72fe019685b30df8368c3923da13a272eb352d2
bgfelix2005 <136112554+bgfelix2005@users.noreply.github.com>
2025-01-20 09:21:10 +0200

    2025-01-20

---
+++ 2024-10-28/quicksort.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+void printArray(int *arr, int size);
+void quickSort(int *arr, int size);
+void quickSortR(int *arr, int l, int r);
+int partition(int *arr, int l, int r);
+
+int main() {
+    int arr[10] = {2,5,1,6,4,8,4,10,3,1 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, size);
+    quickSort(arr, size);
+    printArray(arr, size);
+    return 0;
+}
+
+void swap(int *a, int *b) {
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void printArray(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void quickSort(int *arr, int size) {
+    int l = 0, r = size - 1;
+    quickSortR(arr, l, r);
+}
+
+void quickSortR(int *arr, int l, int r) {
+    if (l >= r) 
+    {
+        return;
+    }    
+    int pivot = partition(arr, l, r);
+    printArray(arr, 10);
+
+    quickSortR(arr, l, pivot-1);   
+    quickSortR(arr, pivot + 1, r);  
+
+    
+}
+
+int partition(int *arr, int l, int r) {
+    int pp = l;
+    int pivot = arr[r];
+    for(int i = l;i<r; i++)
+    {
+        if(arr[i]<pivot)
+        {
+            swap(&arr[i], &arr[pp]);
+            pp++;
+        }                               
+    }  
+    swap(&arr[r], &arr[pp]); 
+    return pp;  
+
+}
